ee243cf7d9fe91a1e7d6a7a86257c403002e4895
jainakhil93 <jainakhil93@gmail.com>
2020-06-16 08:28:05 +1000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_pymongo import PyMongo
+from bson import ObjectId
+import json
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = 'mongodb://127.0.0.1:27017/demo2'
+mongo = PyMongo(app)
+CORS(app)
+db = mongo.db.demo2
+
+
+@app.route('/', methods=['GET', 'POST'])
+def getpost():
+    if request.method == "GET":
+        o = []
+        for i in db.find():
+            o.append({
+                "_ID": str(ObjectId(i["_id"])),
+                "name": i["name"],
+                "email": i["email"],
+                "pasword": i["password"]
+            })
+        return jsonify(o)
+    elif request.method == "POST":
+        id = db.insert({
+            "name": request.json["name"],
+            "email": request.json["email"],
+            "password": request.json["password"]
+        })
+        return jsonify(str(ObjectId(id)))
+
+
+@app.route('/<id>', methods=["DELETE", "PUT"])
+def deleteput(id):
+    if request.method == "DELETE":
+        db.delete_one({"_id": ObjectId(id)})
+        return jsonify({"message": "deleted"})
+    elif request.method == "PUT":
+        db.update({"_id": ObjectId(id)}, {"$set": {
+            "name": request.json["name"],
+            "email": request.json["email"],
+            "password": request.json["password"]
+        }})
+        return jsonify({"message": "updated"})
+
+
+@app.route('/updateDetails/<id>', methods=["GET"])
+def updateDetails(id):
+    res = db.find_one({"_id": ObjectId(id)})
+    print(res)
+    return {"_ID": str(ObjectId(res["_id"])), "name": res["name"], "email": res["email"], "password": res["password"]}
